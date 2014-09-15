280d72407250fed211a71d6445272fb9de327946
Vitaly Sheanov <vit.onix@gmail.com>
2014-09-16 00:36:22 +0400

    add Locatoin

---
+++ src/main/java/net/arwix/astronomy/coordinates/Location.java
@@ -0,0 +1,18 @@
+package net.arwix.astronomy.coordinates;
+
+public class Location {
+
+    public double longitude;
+    public double latitude;
+
+    public Location() {
+
+    }
+
+    public Location(double longitude, double latitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+
+}
